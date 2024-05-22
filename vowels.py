da392b34c8046c0298fc659843a4585d1c903c08
Reuven Lerner <reuven@lerner.co.il>
2024-05-22 14:42:46 -0400

    gitautosync update 73

--- vowels.py
+++ vowels.py
@@ -1,7 +1,12 @@
+print('Hello from vowels!')
+
 def count_vowels(s):
     total = 0
     for one_character in s:
         if one_character in 'aeiou':
             total += 1
     return total
+
+print('Goodbye from vowels!')
+
 
